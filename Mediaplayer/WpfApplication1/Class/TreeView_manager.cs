9abbd57615ad979ce7fe3685e75ef7fd6009a814
raytrac <gastonrivoire>
2015-01-09 19:16:56 +0100

    class treeview_manager empty

---
+++ Mediaplayer/WpfApplication1/Class/TreeView_manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace WpfApplication1
+{
+    class TreeView_manager
+    {
+        int height = 208;
+        int width = 305;
+
+        public TreeView_manager From1(sender e, )
+        {
+            
+        }
+    }
+}
