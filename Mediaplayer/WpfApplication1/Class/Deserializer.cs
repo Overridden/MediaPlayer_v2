f68f8dec568f06b3608f43ab7ca0eb12250c1267
Alexandre Nguyen <alexandre1.nguyen@epitech.eu>
2015-01-12 00:48:41 +0100

    commit 10
    Uploading the new repo:
    -Still bugs on play/pause/stop between direct open and TreeView open
    - Double click & open file from TreeView
    - TreeView deployed & colored in white
    - Open a playlist
    - Save a playlist into Myplaylist2.xml (something like that idc)
    - Fix in progress... (sync between booleans from vm & lm)

---
+++ Mediaplayer/WpfApplication1/Class/Deserializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WpfApplication1
+{
+    class Deserializer
+    {
+        public bool check_file(string file)
+        {
+            string curFile = file;
+            return File.Exists(curFile);
+        }
+
+        public List<Item> deserialize(string file)
+        {
+            List<Item> library_v2;
+
+            XmlSerializer dxs = new XmlSerializer(typeof(List<Item>));
+            using (StreamReader rd = new StreamReader(file))
+            {
+                library_v2 = dxs.Deserialize(rd) as List<Item>;
+            }
+            return library_v2;
+        }
+    }
+}
---
+++ Mediaplayer/WpfApplication1/Class/Serializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WpfApplication1
+{
+    class Serializer
+    {
+        public bool check_file(string file)
+        {
+            string curFile = file;
+            Console.Out.WriteLine("EXIST OR NOT?!" + File.Exists(curFile) + " file " + file);
+            return File.Exists(curFile);
+        }
+
+        public void serialize(List<Item> library, string file)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<Item>));
+            using (StreamWriter wr = new StreamWriter(file))
+            {
+                xs.Serialize(wr, library);
+            }
+        }
+    }
+}
